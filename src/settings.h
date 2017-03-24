a1f1a42f084cafeea8eea8814ac536a57634d858
I33N <petit.benjamin@gmail.com>
2017-03-24 09:11:49 +0100

    Inital project with dual window settings (floor and wall windows) and augmenta test

---
+++ src/settings.h
@@ -0,0 +1,13 @@
+//
+//  settings.h
+//  funambulus
+//
+//  Created by Benjamin Petit on 23/03/2017.
+//
+//
+
+#ifndef settings_h
+#define settings_h
+
+
+#endif /* settings_h */
---
+++ src/window.h
@@ -0,0 +1,13 @@
+//
+//  window.h
+//  funambulus
+//
+//  Created by Benjamin Petit on 23/03/2017.
+//
+//
+
+#ifndef window_h
+#define window_h
+
+
+#endif /* window_h */
